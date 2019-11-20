6fbb08b0f7c365213379006f7337bfa992fe2f20
Capmare <35742735+Capmare@users.noreply.github.com>
2019-11-20 14:19:41 -0800

    The blueprint system alpha
    alpha v1.4

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BluePrints
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RightClick = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.clickToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compilerBtn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.RightClick.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RightClick
+            // 
+            this.RightClick.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clickToolStripMenuItem});
+            this.RightClick.Name = "RightClick";
+            this.RightClick.Size = new System.Drawing.Size(99, 26);
+            // 
+            // clickToolStripMenuItem
+            // 
+            this.clickToolStripMenuItem.Name = "clickToolStripMenuItem";
+            this.clickToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.clickToolStripMenuItem.Text = "click";
+            this.clickToolStripMenuItem.Click += new System.EventHandler(this.clickToolStripMenuItem_Click);
+            // 
+            // compilerBtn
+            // 
+            this.compilerBtn.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.compilerBtn.Location = new System.Drawing.Point(502, 12);
+            this.compilerBtn.Name = "compilerBtn";
+            this.compilerBtn.Size = new System.Drawing.Size(115, 41);
+            this.compilerBtn.TabIndex = 1;
+            this.compilerBtn.Text = "Compile";
+            this.compilerBtn.UseVisualStyleBackColor = false;
+            this.compilerBtn.Click += new System.EventHandler(this.compilerBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::BluePrints.Properties.Resources.grid;
+            this.ClientSize = new System.Drawing.Size(1204, 718);
+            this.ContextMenuStrip = this.RightClick;
+            this.Controls.Add(this.compilerBtn);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1220, 757);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1220, 757);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PythonBlueprints";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.RightClick.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip RightClick;
+        private System.Windows.Forms.ToolStripMenuItem clickToolStripMenuItem;
+        private System.Windows.Forms.Button compilerBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BluePrints
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        GroupBox groupBox = new GroupBox();
+        CheckBox negative = new CheckBox();
+        TextBox textbox = new TextBox();
+        string url;
+
+
+        //mouse location
+        int mouseX = MousePosition.X;
+        int mouseY = MousePosition.Y;
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+           
+        }
+
+        private void init()
+        {
+            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void clickToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            groupBox.Show();
+            groupBox.Enabled = true;
+
+            //label settings
+            Label name = new Label();
+            name.Text = "function1";
+            name.Font = new Font("", 15);
+            name.Location = new Point(60, 0);
+
+            //spawn box at mouse location
+            groupBox.Location = new Point(MousePosition.X - 400, MousePosition.Y - 200);
+
+            //create negative
+            
+            negative.Location = new Point(10, 10);
+            negative.Text = "-";
+            negative.CheckState = CheckState.Checked;
+            negative.ThreeState = false;
+
+            //create close button
+            Button closeBtn = new Button();
+            closeBtn.Location = new Point(65, 60);
+            closeBtn.Text = "Close";
+            closeBtn.PerformClick();
+
+            //create textbox
+            
+            textbox.Location = new Point(50, 35);
+            textbox.Text = url;
+
+            //add to groupBox
+            groupBox.Controls.Add(name);
+            groupBox.Controls.Add(negative);
+            groupBox.Controls.Add(closeBtn);
+            groupBox.Controls.Add(textbox);
+
+            //create group box
+            this.Controls.Add(groupBox);
+
+
+            closeBtn.Click += CloseBtn_Click;
+
+
+
+
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            groupBox.Hide();
+            groupBox.Enabled = false;
+
+        }
+        private static readonly HttpClient client = new HttpClient();
+
+        private void compilerBtn_Click(object sender, EventArgs e)
+        {
+
+            
+            if (negative.CheckState == CheckState.Checked && groupBox.Enabled == true)
+            {
+                GetRequest(textbox.Text);
+
+            }
+            else
+            {
+                
+            }
+        }
+        async static void GetRequest(string urlPost)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(urlPost))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        Console.WriteLine(mycontent);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+}
